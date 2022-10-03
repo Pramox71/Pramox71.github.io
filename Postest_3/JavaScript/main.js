bf1f1fb4401bd3007417043779381c07ec524341
Pramox71 <93047322+Pramox71@users.noreply.github.com>
2022-10-03 23:10:02 +0800

    Delete Postest_3 directory

--- Postest_3/JavaScript/main.js
+++
@@ -1,7 +0,0 @@
-const pesan = document.getElementById('Pesan_Now');
-pesan.addEventListener("click",
-    function(){
-        alert("Mohon maaf, tombol 'Pesan Sekarang!' belum dapat digunakan. Karena Situs dalam masa pengembangan.");
-    },
-)
-
