f5ed2a2f69a472ff3722d2d381e489505f661ac8
Pramox71 <93047322+Pramox71@users.noreply.github.com>
2022-10-03 23:10:32 +0800

    Postest3

---
+++ Postest_3/JavaScript/main.js
@@ -0,0 +1,7 @@
+const pesan = document.getElementById('Pesan_Now');
+pesan.addEventListener("click",
+    function(){
+        alert("Mohon maaf, tombol 'Pesan Sekarang!' belum dapat digunakan. Karena Situs dalam masa pengembangan.");
+    },
+)
+
